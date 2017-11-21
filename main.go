ef6f53c2dc9428687056bcd633854d617f6b8f52
Syd <324760805@qq.com>
2017-11-21 16:26:04 +0800

    1

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"github.com/sydnever/sydlog/src/golang"
-)
-
-func main() {
-	golang.sydlog("hello world")
-}
--- syd/sydlog_go.go
+++ syd/sydlog_go.go
@@ -1,4 +1,4 @@
-package golang
+package syd
 
 import (
 	"log"
