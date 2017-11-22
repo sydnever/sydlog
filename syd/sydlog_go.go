b2d4daf2ba7ed8d78b3e3f7fd75b6e8f5788ddc5
Syd <324760805@qq.com>
2017-11-22 09:36:55 +0800

    color

--- syd/sydlog_go.go
+++ syd/sydlog_go.go
@@ -6,8 +6,13 @@ import (
 )
 
 func Sydlog(str string) {
-	fmt.Print("[DEBUG]")
+	/* log */
+	fmt.Print("\033[31m [DEBUG] \033[0m")
+	fmt.Print("\033[33m ")
 	fmt.Print(time.Now())
-	fmt.Print("  ")
-	fmt.Println(str)
+	fmt.Print(" \033[0m")
+	fmt.Print("\t\t")
+	fmt.Print("\033[34m ")
+	fmt.Print(str)
+	fmt.Println(" \033[0m")
 }
